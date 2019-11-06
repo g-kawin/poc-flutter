cb2b221c125a209df0c47543c44364844e5100b2
Kawin Glomjai <kawin.glomjai@scbabacus.com>
2019-11-06 12:14:32 +0700

    [feature] init github ci

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,6 +19,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
+      title: 'Flutter Demo',
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
